ac12ccd9f902c3b28d8e51ac6c9ca900defa30d4
surendra <surendra.singh_cs20@gla.ac.in>
2022-12-27 17:39:32 +0530

    sign in page using google/facebook

--- src/Pages/Signin.js
+++ src/Pages/Signin.js
@@ -1,20 +1,61 @@
 import React from 'react';
-import { Button, Col, Container, Grid, Icon, Panel, Row } from 'rsuite';
+import { Alert, Button, Col, Container, Grid, Icon, Panel, Row } from 'rsuite';
 
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+} from 'firebase/auth';
+import { app, database } from '../misc/firebase';
+import { ref, set } from 'firebase/database';
+
+const auth = getAuth(app);
 const Signin = () => {
+  const SignInProvider = (auth, provider) => {
+    try {
+      signInWithPopup(auth, provider).then(result => {
+        const user = result.user;
+
+        Alert.success('Signed in');
+        set(ref(database, `/profiles/${user.uid}`), {
+          username: user.displayName,
+          email: user.email,
+          createdAt: user.metadata.creationTime,
+        });
+      });
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
+
+  const onGoogleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    SignInProvider(auth, provider);
+  };
+  const onFacebookSignIn = () => {
+    const provider = new FacebookAuthProvider();
+    console.log(provider);
+    SignInProvider(auth, provider);
+  };
+
   return (
     <Container>
-      <Grid>
+      <Grid className="mt-page">
         <Row>
           <Col xs={24} md={12} mdOffset={6}>
             <Panel>
               <div className="text-center">
                 <h1>Welcome to Chat</h1>
                 <p>Progressive chat platform for neophytes</p>
-
-                <Button block color="blue">
-                  <Icon icon="facebook" /> continue with facebook
-                </Button>
+                <div className="mt-3">
+                  <Button block color="blue" onClick={onFacebookSignIn}>
+                    <Icon icon="facebook" /> Continue with facebook
+                  </Button>
+                  <Button block color="green" onClick={onGoogleSignIn}>
+                    <Icon icon="google" /> Continue with Google
+                  </Button>
+                </div>
               </div>
             </Panel>
           </Col>
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,20 +1,21 @@
-import firebase from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const config = {
+// Your web app's Firebase configuration
+const firebaseConfig = {
   apiKey: 'AIzaSyC8lPEgu2fzcHgd4CwfQ3uxU-iJMN0BpE4',
-
   authDomain: 'chat-app-319dd.firebaseapp.com',
-
   databaseURL:
     'https://chat-app-319dd-default-rtdb.asia-southeast1.firebasedatabase.app',
-
   projectId: 'chat-app-319dd',
-
   storageBucket: 'chat-app-319dd.appspot.com',
-
   messagingSenderId: '384883295980',
-
   appId: '1:384883295980:web:ad7a381ab2ee63f0ef07c9',
 };
 
-const app = firebase.initializeApp(config);
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
